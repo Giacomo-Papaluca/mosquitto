52e461f1a38e7b194f8fd75bb1b665fee113321f
Giacomo-Papaluca <giacomo.papaluca@gmail.com>
2019-11-17 15:50:16 +0100

    Safer config: TLS+PBKDF2

--- tls_test.sh
+++ tls_test.sh
@@ -4,5 +4,5 @@
 for((X=0; X<100; X++))
 
 do
-	./client/mosquitto_pub -p 8883 -t topic --cafile certs/ca.crt -m "Ciao tutto bene?"
+	./client/mosquitto_pub -u Jack -P gOOdmorn1ng -p 8883 -t topic --cafile certs/ca.crt -m "Ciao" --insecure
 done
